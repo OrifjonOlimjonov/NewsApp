b2e01e5a58f683860e9098aef526377d47c742fd
Orifjon <orifjon.olimjonov@mail.ru>
2022-10-19 18:42:55 +0500

    Change a Select Topics Fragment

--- app/src/main/java/uz/orifjon/newsappdagger2/fragments/tutorial/fragments/SelectTopicsFragment.kt
+++ app/src/main/java/uz/orifjon/newsappdagger2/fragments/tutorial/fragments/SelectTopicsFragment.kt
@@ -5,23 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import uz.orifjon.newsappdagger2.R
 import uz.orifjon.newsappdagger2.databinding.FragmentSelectTopicsBinding
 import uz.orifjon.newsappdagger2.utils.viewBindings
 
-class WelcomeFragment : Fragment() {
+class SelectTopicsFragment : Fragment(R.layout.fragment_select_topics) {
 
 
     private val binding by viewBindings(FragmentSelectTopicsBinding::bind)
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
 
-
-
-
-
-
-
-        return binding.root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.apply {
+            btnAnimals.setOnClickListener {
+                //TODO: Select animals
+            }
+            btnArt.setOnClickListener {
+                //TODO: Select art
+            }
+            btnFashion.setOnClickListener {
+                //TODO: Select fashion
+            }
+            btnFood.setOnClickListener {
+                //TODO: Select Food
+            }
+            btnGaming.setOnClickListener {
+                //TODO: Select Gaming
+            }
+            btnLife.setOnClickListener {
+                //TODO: Select Life
+            }
+            btnSports.setOnClickListener {
+                //TODO: Select Sports
+            }
+            btnHistory.setOnClickListener {
+                //TODO: Select History
+            }
+            btnNature.setOnClickListener {
+                //TODO: Select Nature
+            }
+        }
     }
 
 
--- app/src/main/java/uz/orifjon/newsappdagger2/fragments/tutorial/fragments/WelcomeScreenFragment.kt
+++ app/src/main/java/uz/orifjon/newsappdagger2/fragments/tutorial/fragments/WelcomeScreenFragment.kt
@@ -3,6 +3,7 @@ package uz.orifjon.newsappdagger2.fragments.tutorial.fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import uz.orifjon.newsappdagger2.R
 import uz.orifjon.newsappdagger2.databinding.FragmentWelcomeScreenBinding
 import uz.orifjon.newsappdagger2.utils.viewBindings
@@ -14,7 +15,11 @@ class WelcomeScreenFragment : Fragment(R.layout.fragment_welcome_screen) {
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
+            binding.apply {
+                startBtn.setOnClickListener{
+                    findNavController().navigate(R.id.selectTopicsFragment)
+                }
+            }
     }
 
 }

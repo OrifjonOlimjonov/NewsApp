268eb2a1566b3162ebd9fef5decf50f186b42131
Orifjon <orifjon.olimjonov@mail.ru>
2022-10-21 15:25:08 +0500

    Add a Bottom Navigation View

--- app/src/main/java/uz/orifjon/newsappdagger2/MainActivity.kt
+++ app/src/main/java/uz/orifjon/newsappdagger2/MainActivity.kt
@@ -1,15 +1,36 @@
 package uz.orifjon.newsappdagger2
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.navigation.findNavController
 import uz.orifjon.newsappdagger2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        sharedPreferences = getSharedPreferences("tutorial", MODE_PRIVATE)
+        editor = sharedPreferences.edit()
+
+
+        val check = sharedPreferences.getBoolean("check", false)
+        editor.putBoolean("check",true).commit()
+       // Toast.makeText(this, check.toString(), Toast.LENGTH_SHORT).show()
+        if (check) {
+            findNavController(R.id.fragmentContainerView).popBackStack()
+            findNavController(R.id.fragmentContainerView).navigate(R.id.mainFragment)
+        } else {
+            findNavController(R.id.fragmentContainerView).popBackStack()
+            findNavController(R.id.fragmentContainerView).navigate(R.id.onBoardingFragment)
+            // findNavController(R.id.fragmentContainerView).navigate()
+        }
 
 
     }
--- app/src/main/java/uz/orifjon/newsappdagger2/fragments/HomePageFragment.kt
+++ app/src/main/java/uz/orifjon/newsappdagger2/fragments/HomePageFragment.kt
@@ -2,17 +2,14 @@ package uz.orifjon.newsappdagger2.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import dagger.Module
 import uz.orifjon.newsappdagger2.R
-import uz.orifjon.newsappdagger2.databinding.FragmentMainBinding
+import uz.orifjon.newsappdagger2.databinding.FragmentHomePageBinding
 import uz.orifjon.newsappdagger2.utils.viewBindings
 
-class MainFragment : Fragment(R.layout.fragment_main) {
+class HomePageFragment : Fragment(R.layout.fragment_home_page) {
 
-    private val binding by viewBindings(FragmentMainBinding::bind)
+    private val binding by viewBindings(FragmentHomePageBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
